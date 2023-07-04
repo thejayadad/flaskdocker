ed874bb9c324dd5f1fff684407262a2cc0e1d874
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-04 17:06:54 -0400

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 import pymongo
 from pymongo import MongoClient
-from flask import render_template
+from flask import render_template, request
 
 
 app = Flask(__name__)
@@ -23,9 +23,23 @@ def index():
 def login():
     return render_template("./login.html", login=False)
 
-@app.route('/classes')
-def classes():
-    return render_template("./classes.html", login=False)
+@app.route('/classes/')
+@app.route('/classes/<term>')
+def classes(term="Summer"):
+    classData = [{"classID":"101","title":"Hot Yoga","instructor":"Jace The Goat", "duration":"30"},
+    {"classID":"102","title":"Meditation Yoga","instructor":"Jada Boo", "duration":"60"},
+    {"classID":"103","title":"Flexible Yoga","instructor":"Flexin With Jazz", "duration":"75"},
+    {"classID":"104","title":"Healing Yoga","instructor":"Jack Rabbit", "duration":"45"}]
+    return render_template("./classes.html", classData=classData, term=term)
+
+@app.route("/enrollment", methods=["GET", "POST"])
+def enrollment():
+    id= request.args.get('classID')
+    title= request.args.get('title')
+    instructor= request.args.get('instructor')
+    return render_template("enrollment.html", enrollment=True, data={"id":id, "title":title,"instructor":instructor})
+
+
 
 
 if __name__=='__main__':
